ae88c1a56b15066d6c8a5612517071ee948029c8
krish21-j <iambakinsquad@gmail.com>
2025-03-28 10:31:10 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+
+const SECRET_KEY = 'your_secret_key';
+
+
 const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' }); 
+};
+
 
 const decrypt = (token) => {
-  // return decoded payload
-}
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    console.error('Invalid or expired token:', error.message);
+    return null; 
+  }
+};
 
 module.exports = {
   encrypt,
   decrypt
-}
+};
